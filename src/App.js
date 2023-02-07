9349366f649d6fb94f0126ebeb8fc62bf61be7e8
Nikola Lukaszewicz <nicola.lukaszewicz1@gmail.com>
2023-02-07 20:12:18 +0100

    feat: progres

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function App() {
-  return;
-  <div>
-    <h1>"Let's get started" </h1>
-    <h2>this is also visible</h2>
-  </div>;
+  return (
+    <div>
+      <h1>"Let's get started" </h1>
+      <h2>this is also visible</h2>
+    </div>
+  );
 }
 
 export default App;
