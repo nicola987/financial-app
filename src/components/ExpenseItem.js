91f74da1041dd5b3352df09ce7e3eaad3c03978c
Nikola Lukaszewicz <nicola.lukaszewicz1@gmail.com>
2023-02-08 22:57:44 +0100

    second commit finance-app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card/Card";
 
-function ExpenseItem() {
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    <Card className="expense-item">
+      <div>
+        <ExpenseDate date={props.date} />
       </div>
-    </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;
